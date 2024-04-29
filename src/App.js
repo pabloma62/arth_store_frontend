26418b26a0ed71a906a3a32102f58a59f121f91e
pabloma62 <pabloma62@gmail.com>
2024-04-29 17:20:20 +0200

    proyecto de Cards para secciones

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import MainLayout from '../src/layout/MainLayout' // Asegúrate de que la ruta sea correcta
-import { Box, Text, Button } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import MainLayout from '../src/layout/MainLayout'
+import { Box, Text, Flex, IconButton } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
 
 function App () {
   const { t } = useTranslation()
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const slides = [
+    {
+      title: 'Slide 1',
+      content: 'Contenido del Slide 1'
+    },
+    {
+      title: 'Slide 2',
+      content: 'Contenido del Slide 2'
+    }
+  ]
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === slides.length - 1 ? 0 : currentSlide + 1)
+  }
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? slides.length - 1 : currentSlide - 1)
+  }
 
   return (
     <MainLayout>
@@ -19,7 +38,34 @@ function App () {
         >
           {t('landing_page.introduction')}
         </Text>
-        <Button colorScheme='blue'>Haz clic aquí</Button>
+      </Box>
+      <Box mt={8}>
+        <Flex alignItems='center' justifyContent='center'>
+          <IconButton
+            bg='red'
+            onClick={prevSlide}
+            aria-label='Slide anterior'
+            variant='ghost'
+            mr={4}
+          />
+          <Box maxW='md' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+            <Box p='6'>
+              <Box mt='1' fontWeight='semibold' as='h4' lineHeight='tight' isTruncated>
+                {slides[currentSlide].title}
+              </Box>
+              <Box>
+                {slides[currentSlide].content}
+              </Box>
+            </Box>
+          </Box>
+          <IconButton
+            bg='red'
+            onClick={nextSlide}
+            aria-label='Siguiente slide'
+            variant='ghost'
+            ml={4}
+          />
+        </Flex>
       </Box>
     </MainLayout>
   )
